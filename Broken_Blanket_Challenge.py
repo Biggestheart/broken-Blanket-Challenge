86bccff2798e0637d0d220009a250f5b2ea9a456
Anietie Lawrence Hanson Essien <arrhnnie@gmail.com>
2023-01-16 23:16:33 +0100

    Add files via upload

---
+++ Broken_Blanket_Challenge.py
@@ -0,0 +1,12 @@
+''' created by :AnietieLEssien
+    Date:16-Jan-2023
+    created for: challenge mentioned in the        comment'''
+
+print(' '*3+"□•"*11)
+for i in range(1,9):
+    print('■○'*(11-i)+'□●',end=" ")
+    print(" ",end=" ")
+    print('□●'+'■○'*(i-1),end=" ")
+    print()
+print('■○'*2+'□●'+"   "+'□•'*9)
+print("□•"*2)
